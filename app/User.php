328d1992a49f59d155801678f4316bc61584431a
bgcAndroid <33119870+bgcAndroid@users.noreply.github.com>
2018-04-12 09:33:23 +0530

    Update User.php

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'lastName','email', 'password','mobile','address','verifyToken', 
+        'name', 'lastName','email', 'password','mobile','address',
     ];
 
     /**
